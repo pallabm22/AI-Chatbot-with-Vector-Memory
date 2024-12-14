c37491d30d7284675c2a9c06ba67cf09695a23ad
Pallab Mandal <124736661+pallabm22@users.noreply.github.com>
2024-12-14 16:34:59 +0530

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
